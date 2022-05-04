c7a63144dbc456f3a69d0c1c30476a8c7a3abbe8
zshiftonline <zshiftonline@gmail.com>
2022-05-04 19:38:10 -0400

    blah

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
 # unstow config files
 stow -D ohmyzsh zsh nvim vim neofetch htop
 # change directory to avoid conflict
-cd ..
+cd $HOME
+# remove remaining linked files
+rm .vimrc .zshrc .zshrc.bck
+# remove remaining linked .config files
+cd $HOME/.config
+rm -rf gitmoodules htop neofetch nvim ohmyzsh
 # remove dotfiles dir
+cd $HOME
 rm -rf dotfiles/

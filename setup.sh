7b63292551e00137a1a3d72eb58d40196ea944da
zshiftonline <zshiftonline@gmail.com>
2022-05-04 23:28:28 +0000

    blah

--- setup.sh
+++ setup.sh
@@ -1,1 +1,19 @@
-apt update; apt install zsh git stow curl wget htop neofetch neovim vim; apt full-upgrade; git clone --recurse-submodules https://github.com/zshiftonline/dotfiles; cd dotfiles; stow zsh ohmyzsh nvim vim htop neofetch; source ~/.zshrc
+#!/bin/bash
+# update cache
+apt update
+# install dependencies
+apt install zsh git stow curl wget htop neofetch neovim vim
+# system update
+apt full-upgrade
+# clone config repo
+git clone --recurse-submodules https://github.com/zshiftonline/dotfiles
+# cd into repo
+cd dotfiles
+# link config files to system from repo
+stow zsh ohmyzsh nvim vim htop neofetch
+# remove default .zshrc; replace with repo .zshrc
+if [ -f $HOME/.zshrc ]; then
+    mv $HOME/.zshrc.bck $HOME/.zshrc
+fi
+# source newly created shell config
+source ~/.zshrc

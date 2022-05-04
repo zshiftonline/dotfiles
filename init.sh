05afd94823b4e5ed710822ba9664655df67c4760
zshiftonline <zshiftonline@gmail.com>
2022-05-04 10:14:14 -0400

    blah

--- init.sh
+++ init.sh
@@ -2,12 +2,13 @@
 
 ###  Check for config files && Back them up   ###
 
-DIR="ohmyzsh nvim htop neofetch"
-for val in $DIR; do
+declare -a \
+    DIR=("ohmyzsh" "nvim" "htop" "neofetch")
+
+for val in ${DIR[@]}; do
     if [ ! -d "$HOME/.config/ohmyzsh" ]; then
         mkdir -p $HOME/.config/ohmyzsh
     fi
-done
 
 if [ -f "$file" ]; then
     mv $file $file.bak

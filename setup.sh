1b764957becc252c42871a51cf952e74b9a16635
zshiftonline <zshiftonline@gmail.com>
2022-05-04 19:59:11 -0400

    blah

--- setup.sh
+++ setup.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 # update cache
+echo "DEBUG: apt update start"
 apt update
+echo "DEBUG: apt update end"
 # install dependencies
+echo "DEBUG: install deps start"
 apt install zsh git stow curl wget htop neofetch neovim vim
+echo "DEBUG: install deps end"
 # system update
+echo "DEBUG: apt full-upgrade start"
 apt full-upgrade
+echo "DEBUG: apt full-upgrade end"
 # clone config repo
+echo "DEBUG: clone dotfiles repo start"
 cd $HOME
 if [ ! -d $HOME/dotfiles/ ]; then
+    echo "DEBUG: dotfiles dir doesnt exist"
     git clone --recurse-submodules https://github.com/zshiftonline/dotfiles
 else   
+    echo "DEBUG: dir exist and change dir"
     # cd into repo
     cd dotfiles
 fi
+echo "DEBUG: clone dotfiles repo end"
 # link config files to system from repo
+echo "DEBUG: stow configs start"
 stow zsh ohmyzsh nvim vim htop neofetch
+echo "DEBUG: stow configs end"
 # remove default .zshrc; replace with repo .zshrc
+echo "DEBUG: remove .zshrc.bck  & replace if exist start"
 if [ ! -f $HOME/.zshrc.bck ]; then
+    echo "DEBUG: .zshrc.bck doesnt exist"
     source $HOME/.zshrc
 else
-    mv $HOME/.zshrc.bck $HOME/.zshrc
+    echo "DEBUG: .zshrc.bck exists"
+    mv $HOME/.zshrc.bck $HOME/.zshrc && echo "DEBUG: .zshrc replaced"
     source $HOME/.zshrc
 fi
+echo "DEBUG: remove .zshrc.bck & replace if exist end"

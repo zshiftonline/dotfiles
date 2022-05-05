122c75756b4e51b2307b7b990860200993d077b7
zshiftonline <zshiftonline@gmail.com>
2022-05-04 20:00:50 -0400

    blah

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@
 # unstow config files
 stow -D ohmyzsh zsh nvim vim neofetch htop
 # change directory to avoid conflict
-cd $HOME
+cd ..
 # remove remaining linked files
 rm .vimrc .zshrc .zshrc.bck
 # remove remaining linked .config files

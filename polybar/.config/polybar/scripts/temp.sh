2d371570e5bf229baed5602a60c0fb6c1da40674
dustin boyden <zshiftonline@gmail.com>
2023-03-26 03:02:25 -0400

    blah

--- polybar/.config/polybar/scripts/temp.sh
+++ polybar/.config/polybar/scripts/temp.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-TEMP="$(sensors | grep CPU: | awk '{print substr($2,2,4)}')"
-echo -e "$TEMP"
+CPU="$(sensors | grep CPU: | awk '{print substr($2,2,4)}')"
+CPU2="$(sensors | grep CPU_D | awk '{print substr($2,2,4)}')"
+MB="$(sensors | grep MB_ | awk '{print substr($2,2,4)}')"
+GPU="$(sensors | grep GPU_D | awk '{print substr($2,2,4)}')"
+
+echo -e "  %{F#E58F65}CPU%{F-}  $CPU°/$CPU2°  |  %{F#E58F65}MB%{F-}  $MB°  |  %{F#E58F65}GPU%{F-}  $GPU°"
+
 

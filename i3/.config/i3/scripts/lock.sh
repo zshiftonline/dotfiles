ec13d014a49f468c87535dbfa8be4644bebea937
dustin boyden <zshiftonline@gmail.com>
2023-04-21 09:41:45 -0400

    blah

--- i3/.config/i3/scripts/lock.sh
+++ i3/.config/i3/scripts/lock.sh
@@ -5,44 +5,56 @@ BLANK='#00000000'
 CLEAR='#b58db6'
 DEFAULT='#bd6c90cc'
 TEXT='#b3e774aa'
-WRONG='#880000bb'
-VERIFYING='#bb00bbbb'
+WRONG='#dd000088'
+VERIFYING='#227755bb'
 
 i3lock \
---insidever-color=$CLEAR     \
---ringver-color=$VERIFYING   \
+--ignore-empty-password       \
+--insidever-color=$CLEAR      \
+--ringver-color=$VERIFYING    \
 \
---insidewrong-color=$CLEAR   \
---ringwrong-color=$WRONG     \
+--insidewrong-color=$TEXT     \
+--ringwrong-color=$TEXT       \
 \
---inside-color=$BLANK        \
---ring-color=$DEFAULT        \
---line-color=$BLANK          \
---separator-color=$DEFAULT   \
+--inside-color=$BLANK         \
+--ring-color=$DEFAULT         \
+--line-color=$BLANK           \
+--separator-color=$DEFAULT    \
 \
---verif-color=$TEXT          \
---wrong-color=$TEXT          \
---time-color=$TEXT           \
---date-color=$TEXT           \
---greeter-color=$TEXT           \
---layout-color=$TEXT         \
---keyhl-color=$WRONG         \
---bshl-color=$WRONG          \
+--verif-color=$TEXT           \
+--wrong-color=$TEXT           \
+--time-color=$TEXT            \
+--date-color=$TEXT            \
+--greeter-color=$WRONG        \
+--layout-color=$TEXT          \
+--bshl-color=$TEXT            \
 \
---color=101010aa             \
---no-verify \
---screen=1                   \
---clock                      \
---bar-indicator                  \
---time-str="%H:%M:%S"        \
---date-str="%A, %Y-%m-%d"    \
---time-size=70               \
---date-size=$FONT            \
---layout-size=$FONT          \
---verif-size=$FONT           \
---wrong-size=$FONT           \
---greeter-size=$FONT         \
---radius 220                 \
---time-pos="w/4:h/3"        \
---date-pos="w/3:h/3"        \
---greeter-text="TESING"
+--color=08080844              \
+--screen=1                    \
+--clock                       \
+--time-str="%H:%M:%S %P"      \
+--date-str="%A, %B %d, %Y"    \
+--time-size=70                \
+--date-size=$FONT             \
+--layout-size=$FONT           \
+--verif-size=$FONT            \
+--wrong-size=$FONT            \
+--greeter-size=30             \
+--radius 220                  \
+--time-pos="w-(w/4):h-(h/4)"            \
+--date-pos="tx+(w*.01):ty+(h*.05)"      \
+--greeter-pos="tx+(w*.02):ty-(h*.07)"   \
+--greeter-text="Locked"                 \
+--bar-indicator                         \
+--bar-pos y+h                           \
+--bar-direction 1                       \
+--bar-max-height 15                     \
+--bar-base-width 15                     \
+--bar-color 10101077                    \
+--keyhl-color 888888cc                  \
+--bar-periodic-step 20                  \
+--bar-step 20                           \
+--redraw-thread                         \
+--verif-text="verifying..."             \
+--wrong-text="incorrect password"       \
+--noinput-text="invalid password"       \
